e48d0023fa0fe9b9eb0cfbfe991f23086b33f934
Stanislav Ishkov <htrix15@gmail.com>
2025-08-21 22:05:11 +0300

    Add EventAggregator.Shared proj

---
+++ EventAggregator.Shared/ShowEntities/Enums/ShowType.cs
@@ -0,0 +1,18 @@
+﻿namespace EventAggregator.Shared.ShowEntities.Enums;
+
+public enum ShowType
+{
+    Theater,
+    Concert,
+    Sport,
+    Festival,
+    Education,
+    Museum,
+    Show,
+    Children,
+    Tourism,
+    Quests,
+    MasterClasses,
+    Other,
+    Unidentified,
+}
---
+++ EventAggregator.Shared/ValueObjects/DateRange.cs
@@ -0,0 +1,16 @@
+﻿namespace EventAggregator.Shared.ValueObjects;
+
+public readonly struct DateRange
+{
+    public DateOnly Start { get; }
+    public DateOnly End { get; }
+
+    public DateRange(DateOnly start, DateOnly end)
+    {
+        if (start > end)
+            throw new ArgumentException("Start date must be before end date");
+
+        Start = start;
+        End = end;
+    }
+}
