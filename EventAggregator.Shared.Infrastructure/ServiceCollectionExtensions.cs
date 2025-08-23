6c60027a1535060d9177f973a13b10a724e3c27e
Stanislav Ishkov <htrix15@gmail.com>
2025-08-23 23:18:02 +0300

    init EventAggregator.Shared.Infrastructure, add ExternalServiceHealthChecker

---
+++ EventAggregator.Shared.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using EventAggregator.Shared.ExternalServices.Services;
+using EventAggregator.Shared.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventAggregator.Shared.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSharedInfrastructure(this IServiceCollection services)
+    {
+        services.AddTransient<IExternalServiceHealthChecker, ExternalServiceHealthChecker>();
+        return services;
+    }
+}
---
+++ EventAggregator.Shared.Infrastructure/Services/ExternalServiceHealthChecker.cs
@@ -0,0 +1,20 @@
+﻿using EventAggregator.Shared.ExternalServices.Services;
+
+namespace EventAggregator.Shared.Infrastructure.Services;
+
+public class ExternalServiceHealthChecker(IHttpClientFactory httpClientFactory) : IExternalServiceHealthChecker
+{
+    public async Task<bool> IsServiceHealthy(string serviceName, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var client = httpClientFactory.CreateClient(serviceName);
+            var response = await client.GetAsync(WebApplications.Constants.Endpoints.HealthCheckEndpoint, cancellationToken);
+            return response.IsSuccessStatusCode;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
