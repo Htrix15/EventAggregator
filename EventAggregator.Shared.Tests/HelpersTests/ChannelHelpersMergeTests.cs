c2068ca9f0aef47ec2bf809ad56cf8a447ebaf43
Stanislav Ishkov <htrix15@gmail.com>
2025-09-09 21:58:32 +0300

    Add Merge 2 Channel Readers helpers

---
+++ EventAggregator.Shared.Tests/HelpersTests/ChannelHelpersMergeTests.cs
@@ -0,0 +1,184 @@
+﻿using EventAggregator.Shared.Helpers;
+using EventAggregator.Shared.MessageBrokers.Abstractions;
+using EventAggregator.Shared.ShowEntities.Messages;
+using System.Threading.Channels;
+
+namespace EventAggregator.Shared.Tests.HelpersTests;
+
+[TestFixture]
+internal class ChannelHelpersMergeTests
+{
+    [Test]
+    public async Task MergeAsync_EmptyChannels_CompletesImmediately()
+    {
+        // Arrange
+        var channel1 = Channel.CreateBounded<StartShowAggregationMessage>(2);
+        var channel2 = Channel.CreateBounded<BreakShowAggregationMessage>(4);
+
+        channel1.Writer.Complete();
+        channel2.Writer.Complete();
+
+        // Act
+        var result = new List<IMessage>();
+        await foreach (var item in ChannelHelpers.MergeAsync(channel1.Reader, channel2.Reader))
+        {
+            result.Add(item);
+        }
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public async Task MergeAsync_OneChannelFails_ThrowsException()
+    {
+        // Arrange
+        var channel1 = Channel.CreateBounded<StartShowAggregationMessage>(2);
+        var channel2 = Channel.CreateBounded<BreakShowAggregationMessage>(4);
+
+        var channel1Writer = new ValueTask();
+        var channel2Writer = new ValueTask();
+
+        for (int i = 0; i < 2; i++)
+        {
+            channel1Writer = channel1.Writer.WriteAsync(new StartShowAggregationMessage() {
+                RequestDateTime = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+                SearchDateRanges = [],
+                ShowTypes = []
+            });
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            channel2Writer = channel2.Writer.WriteAsync(new BreakShowAggregationMessage()
+            {
+                RequestDateTime = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+            });
+        }
+
+        await channel1Writer;
+        await channel2Writer;
+
+        channel1.Writer.Complete();
+        channel2.Writer.Complete(new InvalidOperationException("Test error"));
+
+        var mergedReaders = ChannelHelpers.MergeAsync(channel1.Reader, channel2.Reader);
+
+        // Assert
+        var exception = Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            // Act
+            await foreach (var item in mergedReaders)
+            {
+            }
+        });
+
+        Assert.That(exception.Message, Is.EqualTo("Test error"));
+    }
+
+    [Test]
+    public async Task MergeAsync_MultipleChannels_MergesAllItems()
+    {
+        // Arrange
+        var channel1 = Channel.CreateBounded<StartShowAggregationMessage>(2);
+        var channel2 = Channel.CreateBounded<BreakShowAggregationMessage>(4);
+
+        var channel1Writer = new ValueTask();
+        var channel2Writer = new ValueTask();
+
+        List<StartShowAggregationMessage> startMessages = [
+            new StartShowAggregationMessage()
+            {
+                RequestDateTime = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+                SearchDateRanges = [],
+                ShowTypes = []
+            },
+            new StartShowAggregationMessage()
+            {
+                RequestDateTime = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+                SearchDateRanges = [],
+                ShowTypes = []
+            }];
+
+        foreach (var item in startMessages) {
+            channel1Writer = channel1.Writer.WriteAsync(item);
+        }
+
+        List<BreakShowAggregationMessage> breakMessages = [
+            new BreakShowAggregationMessage()
+            {
+                RequestDateTime = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+            },
+            new BreakShowAggregationMessage()
+            {
+                RequestDateTime = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+            },
+            new BreakShowAggregationMessage()
+            {
+                RequestDateTime = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+            },
+            new BreakShowAggregationMessage()
+            {
+                RequestDateTime = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+            }];
+
+        foreach (var item in breakMessages)
+        {
+            channel2Writer = channel2.Writer.WriteAsync(item);
+        }
+
+        await channel1Writer;
+        await channel2Writer;
+
+        channel1.Writer.Complete();
+        channel2.Writer.Complete();
+
+        var mergedReaders = ChannelHelpers.MergeAsync(channel1.Reader, channel2.Reader);
+
+        // Assert
+        var result = new List<IMessage>();
+        await foreach (var item in mergedReaders)
+        {
+            result.Add(item);
+        }
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Where(r => r is StartShowAggregationMessage).ToList(), Has.Count.EqualTo(startMessages.Count));
+            Assert.That(result.Where(r => r is BreakShowAggregationMessage).ToList(), Has.Count.EqualTo(breakMessages.Count));
+        });
+    }
+
+    [Test]
+    public async Task MergeAsync_CancelBeforeStart_ThrowsTaskCanceledException()
+    {
+        // Arrange
+        var channel1 = Channel.CreateBounded<StartShowAggregationMessage>(2);
+        var channel2 = Channel.CreateBounded<BreakShowAggregationMessage>(4);
+
+        using var cancellationTokenSource = new CancellationTokenSource();
+
+
+        // Act
+        cancellationTokenSource.Cancel();
+        var mergedReaders = ChannelHelpers.MergeAsync(channel1.Reader, channel2.Reader, mainCancellationToken: cancellationTokenSource.Token);
+
+        // Assert
+        var exception = Assert.ThrowsAsync<TaskCanceledException>(async () =>
+        {
+            await foreach (var item in mergedReaders)
+            {
+            }
+        });
+
+        Assert.That(exception.CancellationToken, Is.EqualTo(cancellationTokenSource.Token));
+    }
+}
---
+++ EventAggregator.Shared/Helpers/ChannelHelpers.cs
@@ -0,0 +1,102 @@
+﻿using EventAggregator.Shared.MessageBrokers.Abstractions;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace EventAggregator.Shared.Helpers;
+
+public class ChannelHelpers
+{
+    /// <summary>
+    /// Merges 2 IMessage channel readers into a single asynchronous IMessage stream.
+    /// The merged stream completes successfully when all source channels are completed normally,
+    /// but terminates with an exception if any source channel completes with an error.
+    /// </summary>
+    public static async IAsyncEnumerable<IMessage> MergeAsync<TStartActionMessage, TBreakActionMessage>(
+        ChannelReader<TStartActionMessage> startActionMessageChannel,
+        ChannelReader<TBreakActionMessage> breakActionMessageChannel,
+        int bufferSize = 100,
+        BoundedChannelFullMode fullMode = BoundedChannelFullMode.Wait,
+        [EnumeratorCancellation] CancellationToken mainCancellationToken = default)
+        where TStartActionMessage : IMessage
+        where TBreakActionMessage : IMessage
+    {
+        if (startActionMessageChannel == null 
+            || breakActionMessageChannel == null)
+        {
+            yield break;
+        }
+
+        var outputChannel = Channel.CreateBounded<IMessage>(new BoundedChannelOptions(bufferSize)
+        {
+            FullMode = fullMode,
+            SingleReader = true,
+            SingleWriter = false
+        });
+
+        using var channelCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(mainCancellationToken);
+        var completionTasks = new Task[2];
+
+        try
+        {
+            completionTasks[0] = ReadFromChannelAsync(startActionMessageChannel, outputChannel.Writer, channelCancellationTokenSource);
+            completionTasks[1] = ReadFromChannelAsync(breakActionMessageChannel, outputChannel.Writer, channelCancellationTokenSource);
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    await Task.WhenAll(completionTasks);
+                }
+                finally
+                {
+                    outputChannel.Writer.TryComplete();
+                }
+            }, mainCancellationToken);
+
+            await foreach (var item in outputChannel.Reader.ReadAllAsync(mainCancellationToken))
+            {
+                yield return item;
+            }
+        }
+        finally
+        {
+            if (completionTasks.Length > 0)
+            {
+                try
+                {
+                    await Task.WhenAll(completionTasks).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    //TODO Log
+                }
+                finally
+                {
+                    completionTasks = null;
+                }
+            }
+        }
+    }
+
+    private static async Task ReadFromChannelAsync<T>(
+        ChannelReader<T> reader,
+        ChannelWriter<IMessage> writer,
+        CancellationTokenSource cancellationTokenSource) where T : IMessage
+    {
+        try
+        {
+            await foreach (var item in reader.ReadAllAsync(cancellationTokenSource.Token))
+            {
+                await writer.WriteAsync(item, cancellationTokenSource.Token);
+            }
+        }
+        catch (OperationCanceledException) when (cancellationTokenSource.Token.IsCancellationRequested)
+        {
+        }
+        catch (Exception ex)
+        {
+            cancellationTokenSource.Cancel();
+            writer.TryComplete(ex);
+            throw;
+        }
+    }
+}
